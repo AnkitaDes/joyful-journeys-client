76ee7bcf72cf07d5e224edc8ba8d3645a85764ed
AnkitaDes <desai_ankita@outlook.com>
2024-02-22 18:44:01 -0500

    Add Create Memory functionality to HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,15 +1,29 @@
 import "./HomePage.scss";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import CreateMemoryModal from "../../components/CreateMemoryModal/CreatMemoryModal";
 import MemoryCard from "../../components/MemoryCard/MemoryCard";
 import { useMemory } from "../../context/MemoryContextProvider";
 
 const HomePage = () => {
   const { memories } = useMemory();
-  console.log(memories);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleMemoryCreated = () => {
+    setIsModalOpen(false); // Close the modal
+    navigate("/profile"); // Navigate to UserProfile page
+  };
 
   return (
     <div>
       <h1>Home Page</h1>
-      <p>Welcome to the Home Page</p>
+      <button onClick={() => setIsModalOpen(true)}>Create Memory</button>
+      <CreateMemoryModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onMemoryCreated={handleMemoryCreated}
+      />
       {memories &&
         memories.map((memory) => (
           <MemoryCard key={memory.id} memory={memory} />
